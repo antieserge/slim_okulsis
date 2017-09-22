56bdb061a9966edf0cb3be29160485d9c0aa5414
kuisatz <kuisatzhaderah@gmail.com>
2017-09-22 14:56:17 +0300

    ilk yukleme

---
+++ vendor/services/database/postgresql/PostgreSQLConnectPDO.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *  Framework 
+ *
+ * @link       
+ * @copyright Copyright (c) 2017
+ * @license   
+ */
+
+namespace Services\Database\Postgresql;
+
+
+/**
+ * service manager layer for database connection
+ * @author Okan CIRAN
+ */
+class PostgreSQLConnectPDO implements \Zend\ServiceManager\FactoryInterface {
+    
+    /**
+     * service ceration via factory on zend service manager
+     * @param \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
+     * @return boolean|\PDO
+     */
+    public function createService(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator) {
+        try {
+            $pdo = new \PDO('pgsql:dbname=development;host=localhost;',
+                            'postgres',
+                            '1Qaaal123',
+                            PostgreSQLConnectPDOConfig::getConfig());
+            return $pdo;
+        } catch (PDOException $e) {
+            return false;
+        } 
+
+
+    }
+
+}
